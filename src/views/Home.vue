42c38a4928eb293a14b8c6bf9c1bf5eedee79bfd
Oleg <olegnsk@yahoo.com>
2022-12-10 23:51:35 -0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>HOME</h2>
+    <p>fdgsdfgsdfgsdfgdfgdfg</p>
+    <a href="/todos">Todos</a>
+  </div>
+</template>
+>
---
+++ src/views/Todos.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>What to do today, to be suceeded tomorrow</h2>
+    <AddTodo @add-todo="addTodo" />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList';
+import AddTodo from '@/components/AddTodo';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json;
+      });
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+  components: {
+    TodoList,
+    AddTodo,
+  },
+};
+</script>
